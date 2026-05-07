4c35e9381105b99e78055c4db156beaa4da54347
Вячеслав Храмцов <sxramsov@gmail.com>
2026-05-07 13:08:08 +0300

    Добавлены тестовые данные

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,2 +1,12 @@
-14 Вариант
-Храмцов Вячеслав Дмитриевич
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #14\n";
+    cout << "Author: Храмцов Вячеслав\n";
+    cout << "Group: 25ИСиТ1-д\n";
+    return 0;
+}
